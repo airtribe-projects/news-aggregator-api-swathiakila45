48f4ec935a510d6cb7a994c8d3e335d494290311
Swathi M <59991732+swathiakila45@users.noreply.github.com>
2025-07-23 21:01:10 +0530

    Basic structure for news aggregator

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+require('dotenv').config(); 
 const express = require('express');
 const app = express();
 const port = 3000;
+const usersRouter = require('./routes/usersRoutes');
+const mongoose = require('mongoose')
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/', usersRouter)
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
-});
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    app.listen(port, (err) => {
+        if (err) {
+            return console.log('Something bad happened', err);
+        }
+        console.log(`Server is listening on ${port}`);
+    });
+})
+
+
 
 
 
---
+++ routes/usersRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const jwt = require('jsonwebtoken')
+const router = express.Router();
+router.use(express.json());
+
+const { registerUser, loginUser,getListofPreferences,modifyPreferences,getNewsArticles} = require('../controller/usersController')
+
+
+
+
+//JWT MIDDLEWARE to AUTHORIZE users for secured apis
+const authorisationMiddleware = (req, res, next) => {
+    const headers = req.headers || {};
+    const token = headers.authorization;
+
+    if (!token) {
+        return res.status(400).send({"message": "Token not found"});
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decodedToken) {
+        return res.status(401).send({message: "Invalid Token"});
+    }
+
+    req.user = decodedToken;
+    next();
+}
+
+//Step 2
+router.post('/register', registerUser) //Register a new User 
+router.post('/login', loginUser) // Login of existing user
+
+//Step 3
+router.get('/preferences', [authorisationMiddleware], getListofPreferences) // Get the list of preferences for the logged in user
+router.put('/preferences', [authorisationMiddleware], modifyPreferences) // Update teh preferences for a user
+
+//Step 4
+router.get('/news',getNewsArticles) // get news articles 
+
+module.exports = router;
